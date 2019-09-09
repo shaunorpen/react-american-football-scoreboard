13126a50ee043d78a412fe18b25ddf4b484d95d1
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-09 12:15:03 +0100

    Add scores to component state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  
+  const [lionsScore, setLionsScore] = useState(0);
+  const [tigersScore, setTigersScore] = useState(0);
 
   return (
     <div className="container">
@@ -15,12 +18,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{lionsScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{tigersScore}</div>
           </div>
         </div>
         <BottomRow />
