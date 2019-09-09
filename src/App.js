fa17046904f6d7f29334c76227b9cf22e9c7e72c
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-09 12:15:58 +0100

    Render scores to the DOM

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   
-  const [lionsScore, setLionsScore] = useState(0);
-  const [tigersScore, setTigersScore] = useState(0);
+  const [lionsScore, setLionsScore] = useState(3);
+  const [tigersScore, setTigersScore] = useState(7);
 
   return (
     <div className="container">
